5ea83acad17f01400807c6f431f30dcc0d9c07f8
Sean Chamberlain <sean.chamberlain@ailytic.com>
2018-02-20 14:21:54 +1030

    Remove boilerplate files

--- src/awesomeModule.js
+++
@@ -1,8 +0,0 @@
-/**
- *
- *   aware of usage `export default` for commonjs modules
- *   according to specs of new es6 modules `export default <Statement>`
- *   will be available in commonjs under `require('module').default` name
- *   it will not work as `require('module')`
- */
-export function awesomeFunction(a, b) { return a + b; }
